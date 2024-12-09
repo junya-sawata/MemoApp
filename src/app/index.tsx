cf5b529a755324ec4e147b8d317938f7a7e8644a
junya <junya.sawata0106@gmail.com>
2024-12-09 16:12:48 +0900

    styling memo items

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,52 +5,51 @@ const Index = (): JSX.Element => {
     <View style={styles.container}> {/* body */}
 
 
-      <View> {/* header */}
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+      <View style={styles.header}> {/* header */}
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
-      <View> {/* scroll */}
+      <View> {/* Momo-List */}
 
-        <View>{/* Momo-List */}
+        <View style={styles.memoListItem}>{/* Momo-List1 */}
+          <View> {/* Momo-List-Item_入力文字 */}
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024年12月9日 10:00</Text>
+          </View>
+          <View> {/* Momo-List-Item_削除Button */}
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoListItem}>{/* Momo-List2 */}
 
           <View> {/* Momo-List-Item_入力文字 */}
-            <Text>買い物リスト</Text>
-            <Text>2024年12月9日 10:00</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024年12月9日 10:00</Text>
           </View>
           <View> {/* Momo-List-Item_削除Button */}
             <Text>X</Text>
           </View>
-          <View> {/* Momo-List-Item_入力文字 */}
-            <Text>買い物リスト</Text>
-            <Text>2024年12月9日 10:00</Text>
-          </View>
-          <View> {/* Momo-List-Item_削除Button */}
-            <Text>X</Text>
-          </View>
-          <View> {/* Momo-List-Item_入力文字 */}
-            <Text>買い物リスト</Text>
-            <Text>2024年12月9日 10:00</Text>
-          </View>
-          <View> {/* Momo-List-Item_削除Button */}
-            <Text>X</Text>
-          </View>
-          <View> {/* Momo-List-Item_入力文字 */}
-            <Text>買い物リスト</Text>
-            <Text>2024年12月9日 10:00</Text>
-          </View>
-          <View> {/* Momo-List-Item_削除Button */}
-            <Text>X</Text>
-          </View>
+        </View>
 
+        <View style={styles.memoListItem}>{/* Momo-List3 */}
+
+          <View> {/* Momo-List-Item_入力文字 */}
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2024年12月9日 10:00</Text>
+          </View>
+          <View> {/* Momo-List-Item_削除Button */}
+            <Text>X</Text>
+          </View>
         </View>
 
       </View>
 
-      <View> {/* Add-Button */}
-        <Text>+</Text>
+      <View style={styles.circleButton}> {/* Add-Button */}
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
 
 
@@ -60,11 +59,83 @@ const Index = (): JSX.Element => {
 }
 
 const styles = StyleSheet.create({
-      container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }
+  container:{
+    flex: 1,
+    backgroundColor:'#FFFFFF'
+  },
+
+  header:{
+    backgroundColor:'#467FD3',
+    height:104,
+    justifyContent:'flex-end'
+  },
+
+  headerInner:{
+    alignItems:'center'
+  },
+
+  headerRight:{
+    position:'absolute',
+    right:16,
+    bottom:16,
+    color:'rgba(255,255,255,0.7)'
+  },
+
+  headerTitle:{
+    marginBottom:8,
+    fontSize:22,
+    lineHeight:32,
+    fontWeight:'bold',
+    color:'#ffffff'
+  },
+
+  memoListItem:{
+    backgroundColor:'#ffffff',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    paddingVertical:16,
+    paddingHorizontal:19,
+    alignItems:'center',
+    borderBottomWidth:0.25,
+    borderColor:'rgba(0,0,0,0.15'
+  },
+
+  memoListItemTitle:{
+    fontSize:16,
+    lineHeight:32
+  },
+
+  memoListItemDate:{
+    fontSize:12,
+    lineHeight:16,
+    color:'#848484'
+  },
+
+  circleButton:{
+    width:64,
+    height:64,
+    borderRadius:32,
+    backgroundColor:'#467FD3',
+    justifyContent:'center',
+    alignItems:'center',
+    position:'absolute',
+    right:40,
+    bottom:40,
+    shadowColor:'#000000',
+    shadowOpacity:0.25,
+    shadowRadius:8,
+    shadowOffset:{ width:0,height:8},
+    elevation:8
+  },
+
+  circleButtonLabel:{
+    color:'#ffffff',
+    fontSize:40,
+    lineHeight:40
+  }
+
 })
+
+
 
 export default Index
